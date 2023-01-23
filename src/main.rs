5a04720cc42ba032f9ceb9313b16e3dda770a9b5
Mourad RL CADI <mouradface1998@gmail.com>
2023-01-23 23:57:36 +0100

    save

--- src/main.rs
+++ src/main.rs
@@ -3,22 +3,26 @@ use yew::{
     Context, 
     Renderer,
     html, 
+    InputEvent,
+    Html,
+    KeyboardEvent
 };
 
-enum Event {
+enum Msg {
     Add,
     Update(String),
     Remove(usize),
-    RemoveAll
+    RemoveAll,
+    Nothing
 }
 
 struct Model {
-    input: String,
-    todos: Vec<String>
+    pub input: String,
+    pub todos: Vec<String>
 }
 
 impl Component for Model {
-    type Message = Event;
+    type Message = Msg;
     type Properties = ();
 
     fn create(_: &Context<Self>) -> Self {
@@ -30,21 +34,73 @@ impl Component for Model {
 
     fn update(&mut self, _: &Context<Self>, ev: Self::Message) -> bool {
         match ev {
-            Event::Add => { true },
-            Event::Update(v) => { true },
-            Event::Remove(i) => { true },
-            Event::RemoveAll => { true }
+            Msg::Add => {
+                let s = self.input.clone();
+                self.todos.push(s);
+                self.input = String::new();
+                true
+            },
+            Msg::Update(v) => {
+                self.input = v;
+                true
+            },
+            Msg::Remove(i) => {
+                self.todos.remove(i);
+                true
+            },
+            Msg::RemoveAll => {
+                self.todos.clear();
+                true
+            },
+            Msg::Nothing => {
+                false
+            }
         }
     }
 
-    fn view(&self, ctx: &Context<Self>) -> yew::Html {
+    fn view(&self, ctx: &Context<Self>) -> Html {
         let link = ctx.link();
+ 
+        let oninput_cb = link.callback(|e: InputEvent| {
+            let target = e.target().unwrap();
+
+            let input = target.value_of().as_string().unwrap();
+
+            println!("{}", input);
+
+            Msg::Update(input.clone())
+        });
+ 
+        let onkeypress_cb = link.callback(|e: KeyboardEvent| {
+            let key = e.key();
+
+            if key.as_str() == "Enter" {
+                Msg::Add
+            } else {
+                Msg::Nothing
+            }
+            
+        });
 
         html! {
             <div class="container">
-                <input placeholder="write something" />
-                <p>{ self.count }</p>
-                <button onclick={ link.callback(|_| Msg::Increment) }>{ "increment" }</button>
+                <input 
+                    placeholder="write something"
+                    type="text"
+                    value={self.input.clone()}
+                    oninput={oninput_cb}
+                    onkeypress={onkeypress_cb}
+                />
+
+                <p>{ &self.input }</p>
+
+                <ul>
+                    {
+                        "todos list"
+                    }
+                </ul>
+
+                <button onclick={link.callback(|_| { Msg::RemoveAll })} >{ "Remove all" }</button>
             </div>
         }
     }
